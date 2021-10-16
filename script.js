139dfc3a489c3e5199c9507872eda404aa0c151b
ashish <68690233+asheeeshh@users.noreply.github.com>
2021-10-16 19:55:49 +0530

    Initial commit
    not yet responsive :(

---
+++ script.js
@@ -0,0 +1,13 @@
+var typed = new Typed('.type', {
+    strings: ['Web Developer.', 'Discord Verified Bot Developer.', 'Designer.', 'Pythoneer.'],
+    typeSpeed: 60,
+    backSpeed: 60,
+    loop: true
+})
+
+var typed2 = new Typed('.type2', {
+    strings: ['Websites.', 'Discord Bots.', 'Designs.'],
+    typeSpeed: 60,
+    backSpeed: 60,
+    loop: true
+})
