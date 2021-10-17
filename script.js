c99cd23fd8bf6b0e709fc2efe9060b7b337b5136
Ashish <68690233+asheeeshh@users.noreply.github.com>
2021-10-17 23:27:49 +0530

    made the website responsive yay

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
+const navcont = document.querySelector('.navbar-container');
+const navbar = document.querySelector('.nav-bar');
+const navlinks = document.querySelector('.nav-links');
+const btn = document.getElementById('nav-btn');
+const icon = document.querySelector('.fas.fa-bars');
+
 var typed = new Typed('.type', {
     strings: ['Web Developer.', 'Discord Verified Bot Developer.', 'Designer.', 'Pythoneer.'],
     typeSpeed: 60,
     backSpeed: 60,
     loop: true
+})
+
+btn.addEventListener("click", (e) =>{
+    e.preventDefault();
+    navlinks.classList.toggle("expand");
+    navcont.classList.toggle("expand");
+    navbar.classList.toggle("expand");
+    icon.classList.toggle("fa-times");
 });
